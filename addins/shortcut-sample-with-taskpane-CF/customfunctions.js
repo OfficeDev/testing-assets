dfd1469f971ee0df674b91eaa5d5d00bcd8cf42a
Yun Lu <luyun@microsoft.com>
2020-08-11 10:02:44 -0700

    sample shortcut addin with taskpane and CF

---
+++ addins/shortcut-sample-with-taskpane-CF/customfunctions.js
@@ -0,0 +1,16 @@
+﻿CustomFunctions.associate('GetValue', function() {
+	if (typeof(g_sharedAppData) === 'object') {
+		return g_sharedAppData.value;
+	}
+
+	return null;
+});
+
+CustomFunctions.associate('SetValue', function(value) {
+	if (typeof(g_sharedAppData) === 'object') {
+		g_sharedAppData.value = value;
+		return value;
+	}
+
+	return null;
+});
